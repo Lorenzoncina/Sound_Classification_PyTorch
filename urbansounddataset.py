4676f93e07b304f37d77ecd1566941792f05c4b2
Lorenzo Concina <49268333+Lorenzoncina@users.noreply.github.com>
2022-05-20 10:56:56 +0200

    Create a custom pythorch dataset with the Urban Sound Dataset

---
+++ urbansounddataset.py
@@ -0,0 +1,40 @@
+
+from torch.utils.data import Dataset
+import pandas as pd
+import torchaudio
+import os
+
+class UrbanSOundDataset(Dataset):
+
+    #constructor
+    def __init__(self, annotation_file, audio_dir):
+        self.annotations = pd.read_csv(annotation_file)
+        self.audio_dir = audio_dir
+
+    #len of the dataset
+    def __len__(self):
+        return len(self.annotations)
+
+    #how to get item from our dataset
+    def __getitem__(self, index):
+        audio_sample_path = self._get_audio_sample_path(index)
+        label = self._get_audio_sample_label(index)
+        signal, sr = torchaudio.load(audio_sample_path)
+
+    def _get_audio_sample_path(self,index):
+        fold = f"fold{self.annotations.iloc[index,5]}"
+        path = os.path.join(self.audio_dir, fold, self.annotations.iloc[index,0] )
+        return path
+
+    def _get_audio_sample_label(self, index):
+        return self.annotations.iloc[index,6]
+
+
+if __name__ == "__main__":
+    #path to the annotation file and to the folder with audio data:
+    ANNOTATIONS_FILE =
+    AUDIO_DIR =
+    usd = UrbanSOundDataset(ANNOTATIONS_FILE, AUDIO_DIR)
+
+    print(f"There are {len(usd)} samples in the dataset.")
+    signal, label = usd[0]
